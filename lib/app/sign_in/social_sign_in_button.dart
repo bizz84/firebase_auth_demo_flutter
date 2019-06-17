1c634e1201d87176f0f0eb1ef3be921e1c8ac1cd
bizz84 <bizz84@gmail.com>
2019-06-17 10:03:23 +0100

    Change syntax for default parameters

--- lib/app/sign_in/social_sign_in_button.dart
+++ lib/app/sign_in/social_sign_in_button.dart
@@ -36,8 +36,8 @@ class SignInButton extends CustomRaisedButton {
     @required String text,
     @required Color color,
     @required VoidCallback onPressed,
-    Color textColor: Colors.black87,
-    double height: 50.0,
+    Color textColor = Colors.black87,
+    double height = 50.0,
   }) : super(
           key: key,
           child: Text(text, style: TextStyle(color: textColor, fontSize: 15.0)),
