c431b1082de673840eb3cfa50f8b8153e1fb8978
bizz84 <bizz84@gmail.com>
2020-01-16 14:43:10 +0000

    Pass AppleSignInAvailable argument to integration tests

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -1,5 +1,6 @@
 //import 'package:firebase_auth_demo_flutter/main.dart' as app;
 import 'package:firebase_auth_demo_flutter/main.dart';
+import 'package:firebase_auth_demo_flutter/services/apple_sign_in_available.dart';
 import 'package:firebase_auth_demo_flutter/services/auth_service_adapter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
@@ -10,5 +11,8 @@ void main() {
 
   // Call the `main()` function of the app, or call `runApp` with
   // any widget you are interested in testing.
-  runApp(MyApp(initialAuthServiceType: AuthServiceType.mock));
+  runApp(MyApp(
+    initialAuthServiceType: AuthServiceType.mock,
+    appleSignInAvailable: AppleSignInAvailable(false),
+  ));
 }
